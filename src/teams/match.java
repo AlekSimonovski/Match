001fc6e53012dcc0a0703e9c31e6ce9c82882e2f
Александар Симоновски <alek.simonovski02@gmail.com>
2023-04-18 05:32:24 +0200

    Added extra time and penalties, in case there is a draw on away goals.

--- src/teams/match.java
+++ src/teams/match.java
@@ -44,6 +44,43 @@ public class match {
 			System.out.println("The score in "+team1.city+" was: "+team1.name+" "+team1.goals1+" - "+team2.return2+" "+team2.name);
 		System.exit(0);
 		}
+		if (total_team2 == total_team1 && team1.return1 == team2.return2) {
+			System.out.println("The tie was drawn and proceeds to Extra time.");
+			int team1_extra;
+			System.out.println("Enter the goals "+team1.name+" scores in extra time: ");
+			team1_extra = input.nextInt();
+			int team2_extra;
+			System.out.println("Enter the goals "+team2.name+" scores in extra time: ");
+			team2_extra = input.nextInt();
+			int team1_aet = team1_extra + team1.return1;
+			int team2_aet = team2_extra + team2.goals2;
+			int team1_full = team1_aet + team1.goals1;
+			int team2_full = team2_aet + team2.return2;
+			if (team1_full > team2_full) {
+				System.out.println(team1.name+" won the tie. The score in "+team2.city+" is: "+team2.name+" "+team2_aet+" - "+team1_aet+" "+team1.name+" ("+team2.name+" "+team2_full+" - "+team1_full+" "+team1.name+")");
+			}
+			if (team1_full < team2_full) {
+				System.out.println(team2.name+" won the tie. The score in "+team2.city+" is: "+team2.name+" "+team2_aet+" - "+team1_aet+" "+team1.name+" ("+team2.name+" "+team2_full+" - "+team1_full+" "+team1.name+")");
+			}
+			if (team1_aet == team2_aet && team1.return1 == team2.goals2) {
+				System.out.println("The tie was drawn and proceeds to penalties.");
+				int team1_penalties;
+				int team2_penalties;
+				System.out.println("Enter the amount of penalties scored by "+team1.name+" :");
+				team1_penalties = input.nextInt();
+				System.out.println("Enter the amount of penalties scored by "+team2.name+" :");
+				team2_penalties = input.nextInt();
+				if (team1_penalties > team2_penalties) {
+					System.out.println(team1.name+" won "+team1_penalties+" - "+team2_penalties+" on penalties. \n"+"Full time score after extra time and penalties is: "+team2.name+" "+team2_full+"("+team2_penalties+") - ("+team1_penalties+")"+team1_full+" "+team1.name); 			
+					}
+				if (team1_penalties < team2_penalties) {
+					System.out.println(team2.name+" won "+team2_penalties+" - "+team1_penalties+" on penalties. \n"+"Full time score after extra time and penalties is: "+team2.name+" "+team2_full+"("+team2_penalties+") - ("+team1_penalties+")"+team1_full+" "+team1.name); 			
+					}
+				if (team1_penalties == team2_penalties) {
+					System.out.println("It's not possible to draw on penalties, try again.");
+				}
+			}
+		}
 		else {
 			System.out.println("Error, bad entry.");
 		}		
